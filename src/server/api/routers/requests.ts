c2ed8f48d61b623dbce77b74db7fca635d7c8871
JoeMics <josephmicla@gmail.com>
2023-05-22 13:16:11 -0700

    init commit

---
+++ src/server/api/routers/requests.ts
@@ -0,0 +1,31 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  publicProcedure,
+  protectedProcedure,
+} from "~/server/api/trpc";
+
+export const requestsRouter = createTRPCRouter({
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.request.findMany();
+  }),
+
+  createRequest: protectedProcedure
+    .input(z.object({ message: z.string() }))
+    .mutation(async ({ input, ctx }) => {
+      const user = ctx.session.user;
+      return await ctx.prisma.request.create({
+        data: {
+          message: input.message,
+          userId: user.id,
+        },
+      });
+    }),
+
+  deleteRequest: protectedProcedure
+    .input(z.string())
+    .mutation(async ({ input, ctx }) => {
+      await ctx.prisma.request.delete({ where: { id: input } });
+    }),
+});
